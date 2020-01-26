0f829cc526afc8c725cc94ff3018d5a0df11a40e
Paul Borawski <paulborawski14@gmail.com>
2020-01-26 02:05:27 -0500

    adding canon in d first 8 measures

---
+++ canon-in-d.rb
@@ -0,0 +1,94 @@
+use_bpm 100
+use_synth :tri
+live_loop :intro do
+  play :fs4
+  play :d4
+  play :a3
+  sleep 2
+  play :e4
+  sleep 2
+  play :d4
+  sleep 2
+  play :cs4
+  sleep 2
+  play :b3
+  sleep 2
+  play :a3
+  sleep 2
+  play :b3
+  sleep 2
+  play :cs4
+  sleep 2
+end
+# measure one
+play :d3
+sleep 0.5
+play :a3
+sleep 0.5
+play :d4
+sleep 0.5
+play :fs4
+sleep 0.5
+# measure two
+play :a2
+sleep 0.5
+play :e3
+sleep 0.5
+play :a3
+sleep 0.5
+play :cs4
+sleep 0.5
+# measure three
+play :b3
+sleep 0.5
+play :fs3
+sleep 0.5
+play :b3
+sleep 0.5
+play :d4
+sleep 0.5
+# measure four
+play :fs2
+sleep 0.5
+play :cs3
+sleep 0.5
+play :fs3
+sleep 0.5
+play :a3
+sleep 0.5
+# measure five
+play :g2
+sleep 0.5
+play :d3
+sleep 0.5
+play :g3
+sleep 0.5
+play :b3
+sleep 0.5
+# measure six
+play :d2
+sleep 0.5
+play :a2
+sleep 0.5
+play :d3
+sleep 0.5
+play :fs2
+sleep 0.5
+# measure seven
+play :g2
+sleep 0.5
+play :d3
+sleep 0.5
+play :g3
+sleep 0.5
+play :b3
+sleep 0.5
+# measure eight
+play :a2
+sleep 0.5
+play :e3
+sleep 0.5
+play :a3
+sleep 0.5
+play :cs4
+sleep 0.5
