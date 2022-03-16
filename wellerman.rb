db40cdd989aaa724bb5cf3f08a89797725a7b385
Paul Borawski <paulborawski14@gmail.com>
2022-03-16 12:15:03 -0700

    added wellerman

---
+++ wellerman.rb
@@ -0,0 +1,278 @@
+# Welcome to Sonic Pi
+use_synth :piano
+use_bpm 90
+live_loop :drums do
+  sample :drum_heavy_kick
+  sleep 1
+end
+play :c2, release: 0.5
+sleep 1
+play :c2, release: 0.5
+sleep 1
+play :c2, release: 0.5
+sleep 1
+play :c2, release: 0.5
+sleep 0.50
+use_synth :pluck
+play :bb3, release: 0.5
+sleep 0.50
+# once was a ship
+use_synth :piano
+play :c2, release: 0.5
+use_synth :pluck
+play :c4, release: 0.5
+sleep 0.50
+play :c4, release: 0.5
+sleep 0.25
+play :c4, release: 0.5
+sleep 0.25
+use_synth :piano
+play :c2, release: 0.5
+use_synth :pluck
+play :c4, release: 0.5
+sleep 0.50
+play :eb4, release: 0.5
+sleep 0.50
+use_synth :piano
+play :c2, release: 0.5
+play :g4, release: 0.5
+sleep 0.50
+play :g4, release: 0.5
+sleep 0.50
+use_synth :piano
+play :c2, release: 0.5
+use_synth :pluck
+play :g4, release: 0.5
+sleep 0.50
+play :g4, release: 0.5
+sleep 0.50
+# name of the
+use_synth :piano
+play :c2, release: 0.5
+use_synth :pluck
+play :ab4, release: 0.5
+sleep 0.50
+play :f4, release: 0.5
+sleep 0.25
+play :f4, release: 0.5
+sleep 0.25
+use_synth :piano
+play :c2, release: 0.5
+use_synth :pluck
+play :f4, release: 0.5
+sleep 0.50
+play :g4, release: 0.5
+sleep 0.25
+play :ab4, release: 0.5
+sleep 0.25
+use_synth :piano
+play :c2, release: 0.5
+use_synth :pluck
+play :ab4, release: 0.5
+sleep 0.25
+play :c5, release: 0.5
+sleep 0.25
+play :g4, release: 0.5
+sleep 0.50
+use_synth :piano
+play :c2, release: 0.5
+use_synth :pluck
+play :g4, release: 0.5
+sleep 0.75
+play :g4, release: 0.5
+sleep 0.25
+# winds blew up
+use_synth :piano
+play :c2, release: 0.5
+use_synth :pluck
+play :c4, release: 0.5
+sleep 0.50
+play :c4, release: 0.5
+sleep 0.25
+play :c4, release: 0.5
+sleep 0.25
+play :c2, release: 0.5
+play :c4, release: 0.5
+sleep 0.50
+play :eb4, release: 0.5
+sleep 0.50
+play :c2, release: 0.5
+play :g4, release: 0.5
+sleep 0.50
+play :g4, release: 0.5
+sleep 0.50
+play :c2, release: 0.5
+play :g4, release: 0.5
+sleep 0.50
+play :g4, release: 0.5
+sleep 0.50
+# blow boy
+play :c2, release: 0.5
+play :g3, release: 1
+play :g4, release: 1
+sleep 0.50
+play :f3, release: 1
+play :f4, release: 1
+sleep 0.50
+play :c2, release: 0.5
+play :eb3, release: 1
+play :eb4, release: 1
+sleep 0.50
+play :d3, release: 1
+play :d4, release: 1
+sleep 0.50
+play :c1, release: 1
+play :c2, release: 0.5
+play :c3, release: 1
+sleep 1
+sample :drum_snare_hard
+sleep 1
+# one day the wellerman
+play :ab2, release: 1
+play :bb3, release: 1
+play :eb3, release: 1
+play :ab3, release: 1
+play :c4, release: 1
+play :c5, release: 1
+sleep 1
+play :ab2, release: 1
+play :bb3, release: 1
+play :eb3, release: 1
+play :ab3, release: 1
+play :c4, release: 1
+play :c5, release: 1
+sleep 0.75
+play :ab4, release: 0.5
+sleep 0.25
+play :eb2, release: 1
+play :eb3, release: 1
+play :eb4, release: 1
+play :g4, release: 1
+play :bb4, release: 1
+sleep 0.25
+play :bb4, release: 1
+sleep 0.25
+play :bb2, release: 1
+play :bb3, release: 1
+play :bb4, release: 1
+play :eb4, release: 1
+play :g4, release: 1
+sleep 0.5
+play :bb2, release: 1
+play :bb3, release: 1
+play :bb4, release: 1
+play :eb4, release: 1
+play :g4, release: 1
+sleep 0.75
+play :bb2, release: 1
+play :bb3, release: 1
+play :bb4, release: 1
+play :eb4, release: 1
+play :g4, release: 1
+sleep 0.25
+# bring us
+play :f2, release: 1
+play :f3, release: 1
+play :c4, release: 1
+play :f4, release: 1
+play :ab4, release: 1
+sleep 0.50
+play :c4, release: 1
+play :eb4, release: 1
+play :f4, release: 1
+sleep 0.50
+play :c4, release: 1
+play :d4, release: 1
+play :f4, release: 1
+sleep 0.25
+play :c4, release: 1
+play :d4, release: 1
+play :f4, release: 1
+sleep 0.25
+play :c4, release: 1
+play :d4, release: 1
+play :f4, release: 1
+play :ab4, release: 1
+sleep 0.50
+play :c2, release: 1
+play :c3, release: 1
+play :eb4, release: 1
+play :g4, release: 1
+play :c5, release: 1
+sleep 0.50
+play :eb4, release: 1
+play :g4, release: 1
+sleep 0.50
+play :c2, release: 1
+play :c3, release: 1
+play :eb4, release: 1
+play :g4, release: 1
+sleep 1
+# one day
+play :ab2, release: 1
+play :bb3, release: 1
+play :eb3, release: 1
+play :ab3, release: 1
+play :c4, release: 1
+play :c5, release: 1
+sleep 1
+play :ab2, release: 1
+play :bb3, release: 1
+play :eb3, release: 1
+play :ab3, release: 1
+play :c4, release: 1
+play :c5, release: 1
+sleep 0.50
+play :ab4, release: 0.5
+sleep 0.25
+play :ab4, release: 0.5
+sleep 0.25
+play :eb2, release: 1
+play :eb3, release: 1
+play :eb4, release: 1
+play :g4, release: 1
+play :bb4, release: 1
+sleep 0.25
+play :bb4, release: 1
+sleep 0.25
+play :bb2, release: 1
+play :bb3, release: 1
+play :bb4, release: 1
+play :eb4, release: 1
+play :g4, release: 1
+play :bb4, release: 1
+sleep 0.5
+play :bb2, release: 1
+play :bb3, release: 1
+play :bb4, release: 1
+play :eb4, release: 1
+play :g4, release: 1
+play :bb4, release: 1
+sleep 0.50
+play :bb2, release: 1
+play :bb3, release: 1
+play :bb4, release: 1
+play :eb4, release: 1
+play :g4, release: 1
+play :bb4, release: 1
+sleep 0.50
+# take our leave
+play :c2, release: 0.5
+play :g3, release: 1
+play :g4, release: 1
+sleep 0.50
+play :f3, release: 1
+play :f4, release: 1
+sleep 0.50
+play :c2, release: 0.5
+play :eb3, release: 1
+play :eb4, release: 1
+sleep 0.50
+play :d3, release: 1
+play :d4, release: 1
+sleep 0.50
+play :c1, release: 1
+play :c2, release: 0.5
+play :c3, release: 1
+sleep 1
+sample :drum_snare_hard
